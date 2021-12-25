a5e8798c585f0428c55c0f91b1ee3e491a257408
lotsoftoads <lotsoftoads@yahoo.com>
2021-12-25 19:38:07 +0400

    lab6 fixed!
    6 работа - сейчас у Вас всегда написано на кнопке "Прибавить единичку". Выведите вместо "единичку"  реальное значение шага счётчика. При этом если значение step отрицательное, должно быть написано, например, "Вычесть 10", а не "Прибавить -10".

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import Button from './Button';
-import { useState } from 'react';
+import React from "react";
+import Button from "./Button";
+import { useState } from "react";
 
 interface CounterProps {
-    start: number;
-    step: number;
+  start: number;
+  step: number;
 }
 
 var Counter = (props: CounterProps) => {
-    var [count, setCount] = useState(props.start);
-    var handleIncrease = () => {
-        setCount(count + props.step);
-    };
+  var [count, setCount] = useState(props.start);
+  var handleIncrease = () => {
+    setCount(count + props.step);
+  };
+  if (props.step > 0) {
     return (
-        <div>
-            <div>Значение счётчика: {count}.</div>
-            <Button text={"Прибавить единичку"} onClick={handleIncrease} />
-        </div>
+      <div>
+        <div>Значение счётчика: {count}.</div>
+        <Button text={"Прибавить " + props.step} onClick={handleIncrease} />
+      </div>
     );
+  } else {
+    return (
+      <div>
+        <div>Значение счётчика: {count}.</div>
+        <Button text={"Вычесть " + -1 * props.step} onClick={handleIncrease} />
+      </div>
+    );
+  }
 };
 export default Counter;
