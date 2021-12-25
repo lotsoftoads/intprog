761a2aa1b9cfdf3f9551afd6136cf318b95a1277
lotsoftoads <lotsoftoads@yahoo.com>
2021-12-25 21:01:35 +0400

    lab8 fixed!
    8.1) Рассмотрите более внимаиельно использование хука useEffect. Сейчас он у вас будет вычитывать список репозиториев с веб-сервера несколько раз. А надо, чтобы только первый раз при отрисовке страницы. Надо вызов этого хука немного исправить.
    8.2)  После использования .map при рендере компонентов необходимо использовать атрибут key.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import './App.css';
-import { Box, AppBar, Toolbar, Typography, Grid, Container, Card, CardContent, CardHeader } from '@mui/material';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Grid,
+  Container,
+  Card,
+  CardContent,
+  CardHeader,
+} from "@mui/material";
+import { useState, useEffect } from "react";
 
 // интерфейс с информацией о репозитории: названии, описании, ссылке на github.
 interface GithubRepo {
@@ -9,37 +19,29 @@ interface GithubRepo {
   html_url: string;
 }
 
-// асинхронный метод по получению данных от API при помощи fetch API
-let getData = async () => {
-  return fetch('https://api.github.com/users/mui-org/repos')
-    .then(res => res.json())
-    .then((res: GithubRepo[]) => {
-      res.forEach((repos) => {
-        console.log("name: ", repos.name);
-        console.log("link: ", repos.html_url);
-        console.log("description: ", repos.description);
-        console.log("----------------------");
-      });
-      return res;
-    })
-};
-
 let dataArray: GithubRepo[] = [];
 
 function App() {
-
   // состояние компонента "приложение" - "массив репозиториев, полученный через API"
   const [data, setData] = useState(dataArray);
 
+  // метод по получению данных от API при помощи fetch API
   useEffect(() => {
-    getData().then((res) => {
-      setData(res);
-    });
+    fetch("https://api.github.com/users/mui-org/repos")
+      .then((res) => res.json())
+      .then((res: GithubRepo[]) => {
+        res.forEach((repos) => {
+          console.log("name: ", repos.name);
+          console.log("link: ", repos.html_url);
+          console.log("description: ", repos.description);
+          console.log("----------------------");
+        });
+        setData(res);
+      });
   });
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-
       <AppBar position="relative">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -49,33 +51,25 @@ function App() {
       </AppBar>
 
       <Container>
-
-        <Typography gutterBottom variant="h5" component="h2" sx={{ pt: '5%' }}>
+        <Typography gutterBottom variant="h5" component="h2" sx={{ pt: "5%" }}>
           Репозитории:
         </Typography>
 
-        <Grid container spacing={4} sx={{ pt: '2%' }}>
+        <Grid container spacing={4} sx={{ pt: "2%" }}>
           {data.map((card) => (
-            <Grid item xs={12} sm={6} md={4}>
+            <Grid item key={card.html_url} xs={12} sm={6} md={4}>
               <Card sx={{ minWidth: 100 }}>
-
-                <CardHeader
-                  title={card.name}
-                  subheader={card.html_url}
-                />
+                <CardHeader title={card.name} subheader={card.html_url} />
 
                 <CardContent>
-                  <Typography>
-                    {card.description}
-                  </Typography>
+                  <Typography>{card.description}</Typography>
                 </CardContent>
-
               </Card>
             </Grid>
           ))}
         </Grid>
       </Container>
-    </Box >
+    </Box>
   );
 }
 
